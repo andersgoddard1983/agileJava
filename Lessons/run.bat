2d2c3a3b50b39f5c3af196e49af2ca37ee02ecd8
Andrew Goddard <anders@campaignforalcoholfree.com>
2019-12-17 14:52:06 +0000

    Part way through Chapter 2 - before dates are introduced

--- Lessons/run.bat
+++ Lessons/run.bat
@@ -1,3 +1,3 @@
 @echo off
-javac -classpath U:\JUnit\junit-3.8.1.jar *.java
-if not errorlevel 1 java -cp .;U:\JUnit\junit-3.8.1.jar junit.awtui.TestRunner StudentTest
+javac -classpath U:\JavaProjects\agileJava\Lessons;U:\JUnit\junit-3.8.1.jar studentinfo\*.java
+if not errorlevel 1 java -cp U:\JavaProjects\agileJava\Lessons;U:\JUnit\junit-3.8.1.jar junit.awtui.TestRunner studentinfo.AllTests
---
+++ Lessons/studentinfo/CourseSession.java
@@ -0,0 +1,34 @@
+package studentinfo;
+
+import java.util.ArrayList;
+
+class CourseSession {
+	private String department;
+	private String number;
+	private ArrayList<Student> students = new ArrayList<Student>();
+	
+	CourseSession(String department, String number){
+		this.department = department;
+		this.number = number;
+	}
+	
+	String getDepartment(){
+		return department;
+	}
+	
+	String getNumber(){
+		return number;
+	}
+	
+	int getNumberOfStudents(){
+		return students.size();
+	}
+	
+	void enroll(Student student){
+		students.add(student);
+	}
+	
+	Student get(int index){
+		return students.get(index);
+	}
+}
