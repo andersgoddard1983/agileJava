531a22bbcff7611259e74c7f76be4000c0bb058e
Andrew Goddard <anders@campaignforalcoholfree.com>
2019-12-26 11:58:56 +0000

    Introduced a Grade enum to validate the grades input to the methods and an Honors student

--- Lessons/sis/studentinfo/Student.java
+++ Lessons/sis/studentinfo/Student.java
@@ -1,10 +1,15 @@
 package sis.studentinfo;
 
+import java.util.*;
+
 public class Student {
 	
 	private String name;
 	private int credits;
 	private String state = "";
+	enum Grade {A, B, C, D, F};
+	private ArrayList<Grade> grades = new ArrayList<Grade>();
+	private boolean isHonors = false;
 	static final int CREDITS_REQUIRED_FOR_FULL_TIME = 12;
 	static final String IN_STATE = "CO";
 	
@@ -35,5 +40,39 @@ public class Student {
 	
 	void setState(String state){
 		this.state = state;
+	}
+	
+	void addGrade(Grade grade){
+		grades.add(grade);
+	}
+	
+	double getGpa(){
+		if (grades.isEmpty())
+			return 0.0;
+		double total = 0.0;
+		for (Grade grade: grades){
+			total += gradePointsFor(grade);
+		}
+		return total / grades.size();
+	}
+	
+	int gradePointsFor(Grade grade){
+		int points = basicGradePointsFor(grade);
+		if (isHonors)
+			if (points > 0)
+				points += 1;
+		return points;
+	}
+		
+	private int basicGradePointsFor(Grade grade) { 
+		if (grade == Grade.A) return 4;
+		if (grade == Grade.B) return 3;
+		if (grade == Grade.C) return 2;
+		if (grade == Grade.D) return 1;
+		return 0;
+	}
+	
+	void setHonors() {
+		isHonors = true;
 	}
 }
--- Lessons/sis/studentinfo/StudentTest.java
+++ Lessons/sis/studentinfo/StudentTest.java
@@ -3,6 +3,9 @@ package sis.studentinfo;
 import junit.framework.TestCase;
 
 public class StudentTest extends TestCase {
+	
+	private static final double GRADE_TOLERANCE = 0.05;
+	
 	public void testCreate() {
 		final String firstStudentName = "Jane Doe";
 		Student firstStudent = new Student(firstStudentName);
@@ -35,5 +38,45 @@ public class StudentTest extends TestCase {
 		assertTrue(student.isInState());
 		student.setState("MD");
 		assertFalse(student.isInState());
+	}
+	
+	public void testCalculateGpa(){
+		Student student = new Student("a");
+		assertGpa(student, 0.0);
+		student.addGrade(Student.Grade.A);
+		assertGpa(student, 4.0);
+		student.addGrade(Student.Grade.B);
+		assertGpa(student, 3.5);
+		student.addGrade(Student.Grade.C);
+		assertGpa(student, 3.0);
+		student.addGrade(Student.Grade.D);
+		assertGpa(student, 2.5);
+		student.addGrade(Student.Grade.F);
+		assertGpa(student, 2.0);
+	}
+	
+	public void assertGpa(Student student, double expectedGpa){
+		assertEquals(expectedGpa, student.getGpa(), GRADE_TOLERANCE);
+	}
+	
+	public void testCalculateHonorsStudentGpa(){
+		assertGpa(createHonorsStudent(), 0.0);
+		assertGpa(createHonorsStudent(Student.Grade.A), 5.0);
+		assertGpa(createHonorsStudent(Student.Grade.B), 4.0);
+		assertGpa(createHonorsStudent(Student.Grade.C), 3.0);
+		assertGpa(createHonorsStudent(Student.Grade.D), 2.0);
+		assertGpa(createHonorsStudent(Student.Grade.F), 0.0);
+	}
+	
+	private Student createHonorsStudent(Student.Grade grade){
+		Student student = createHonorsStudent();
+		student.addGrade(grade);
+		return student;
+	}
+	
+	private Student createHonorsStudent(){
+		Student student = new Student("a");
+		student.setHonors();
+		return student;
 	}
 }
